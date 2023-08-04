2d515bfeee315681748d11232e37a4c277e97d10
ondrabimka <ondrabimka@seznam.cz>
2023-08-04 19:53:22 +0200

    updated readme

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import machine
 from machine import ADC
 
 # based on https://peppe8o.com/pulse-sensor-with-raspberry-pi-pico-hearth-beat-chech-with-micropython/
-# setup the Pulse Sensor reading pin
+# create an ADC object on ADC pin 28
 pulse = ADC(28)
 file = open("Pressed.txt", "w")
 x = 0
